3ed3e6deed15ea26ecf1369053598a01ec17c164
Mit Patel <mitjpatel@gmail.com>
2021-11-05 14:04:41 -0500

    Version 3.0.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pilgrim",
-            url: "https://foursquare.jfrog.io/artifactory/pilgrimsdk-ios/v3.0.1/Pilgrim.framework.zip",
-            checksum: "ef6a7eb7b91ba656e247488b030aa14a1d3074b4a033025066460a88b89b66ff"
+            url: "https://foursquare.jfrog.io/artifactory/pilgrimsdk-ios/v3.0.1/Pilgrim.xcframework.zip",
+            checksum: "055aa2d7e390254a25ca2fe7d3da6214c16f544172616ebdddc5815183ade3fe"
         )
     ]
 )
