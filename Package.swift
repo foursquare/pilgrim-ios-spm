f244e641aea5dc7d20df50ad1e93112b9d1a710f
Mit Patel <mitjpatel@gmail.com>
2021-11-05 14:06:05 -0500

    Version 3.0.2-beta2

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pilgrim",
-            url: "https://foursquare.jfrog.io/artifactory/pilgrimsdk-ios/v3.0.1/Pilgrim.xcframework.zip",
-            checksum: "055aa2d7e390254a25ca2fe7d3da6214c16f544172616ebdddc5815183ade3fe"
+            url: "https://foursquare.jfrog.io/artifactory/pilgrimsdk-ios-beta/v3.0.2-beta2/Pilgrim.xcframework.zip",
+            checksum: "172adc877b1d4efac705979b27f93ef4f365468837f3589ee4b8ede507dceeb4"
         )
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# pilgrim-ios-spm
+# Swift Package for Pilgrim SDK
 
-A description of this package.
+Pilgrim SDK is the always on, passive location detection engine by Foursquare.
+
+The Pilgrim SDK provides contextual awareness to mobile applications and connected devices to understand where and how your users are moving through the real world.
+
+## Documentation
+
+Refer to detailed documentation [here](https://developer.foursquare.com/docs/pilgrim-sdk/).
